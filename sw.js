a1b29a11beab357ec588dcf017e11625c8b585e8
Andre Loureiro <andreloureiroo@gmail.com>
2016-05-29 19:03:29 -0300

    PWA!

---
+++ sw.js
@@ -0,0 +1,27 @@
+const cacheName = 'pomocycle-v1'
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/dist/bundle.js',
+  '/css/style.css',
+  '/images/icon.png'
+]
+
+self.addEventListener('install', e => {
+  console.log('[Service Worker] install')
+  e.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        console.log('[Service Worker] caching app')
+        return cache.addAll(filesToCache)
+      })
+  )
+})
+
+self.addEventListener('fetch', e => {
+  console.log('[Service Worker] Fetch', e.request.url)
+  e.respondWith(
+    caches.match(e.request)
+      .then(response => response || fetch(e.request))
+  )
+})
